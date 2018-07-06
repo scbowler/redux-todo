8681d40efcc0f6bdc9702d9f963690fddfd094a3
Scott Bowler <scott.bowler@learningfuze.com>
2018-07-05 18:19:48 -0700

    Completed add funcitonality

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
+import { Route } from 'react-router-dom';
 import List from './list';
+import AddItem from './add_item';
 
 const App = () => (
     <div className="container">
-        <List/>
+        <Route exact path="/" component={List}/>
+        <Route path="/add-item" component={AddItem}/>
     </div>
 );
 
