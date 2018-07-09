5145737eac0ec1b9fd4b088a5ad2f04509e95595
Scott Bowler <scott.bowler@learningfuze.com>
2018-07-09 12:49:25 -0700

    Completed single item view

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import List from './list';
 import AddItem from './add_item';
+import ViewItem from './view_item';
 
 const App = () => (
     <div className="container">
         <Route exact path="/" component={List}/>
         <Route path="/add-item" component={AddItem}/>
+        <Route path="/todo/:item_id" component={ViewItem}/>
     </div>
 );
 
